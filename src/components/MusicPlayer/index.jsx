1517d52fd47db8943873162a27b8e9b8679dbc2a
dimitar <dimitar.daskalov22@gmail.com>
2023-12-30 12:15:38 +0200

    fix: cut the bottom of the player on mobile

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -58,7 +58,7 @@ const MusicPlayer = () => {
   };
 
   return (
-    <div className="relative sm:px-12 px-8 w-full flex items-center justify-between bottom-0">
+    <div className="fixed sm:px-12 px-8 w-full flex items-center justify-between mb-5 bottom-0">
       <Track
         isPlaying={isPlaying}
         isActive={isActive}
